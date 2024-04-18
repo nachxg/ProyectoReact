38fa3b347a4395a67f31bbe21f581a6c6f99324c
Nacho Gimenez <129122200+nachxg@users.noreply.github.com>
2024-04-18 12:52:26 -0300

    CreaTuLanding+Gimenez

--- mi-tienda/src/components/Navbar/Carrito/Carrito.jsx
+++ mi-tienda/src/components/Navbar/Carrito/Carrito.jsx
@@ -3,8 +3,11 @@ import { BsCart3 } from "react-icons/bs";
 
 export const Carrito = () => {
   return (
-    <button>
-        <BsCart3 className='h-6 w-6 md:h-7 md:w-7 md:p-[1px] active:scale-[120%] md:hover:scale-[120%] cursor-pointer'/>
+    <button className='relative active:scale-[120%] md:hover:scale-[120%]'>
+        <BsCart3 className='h-6 w-6 md:h-7 md:w-7 md:p-[1px]'/>
+        <div className='absolute bg-gradient-to-r from-[#d37912] to-[#A83279] flex items-center justify-center size-[17px] rounded-full -top-[5px] -right-[6px] text-white text-sm'>
+          3
+        </div>
     </button>
   )
 }
